86d15b0423047c5e57a911c97eeaf2cc3e8e6995
Makoto Inoue <inouemak@googlemail.com>
2017-03-25 19:31:37 +0000

    Added basic script

--- scripts/customer.js
+++ scripts/customer.js
@@ -1,6 +1,21 @@
 var Market = artifacts.require("Market");
+var customer = process.argv[4];
+var instance;
+console.log('Hi, I am customer', customer);
 
 module.exports = function(callback) {
-  console.log('customer', Market.deployed())
-  callback()
+  Market.deployed().then(function(i){
+    instance = i;
+    return instance.request('0x0', customer, 100)
+  }).then(function(response){
+    events = instance.allEvents({})
+    events.watch(function(err,result) {
+      if (result.event == 'QuoteRespondedEvent') {
+        console.log('Got response from ', result.args.name, 'for $', result.args.premium.toString(), 'Million')
+        callback();
+      }else{
+        // console.log('unrelated', result.event, result.args.name);
+      }
+    })
+  })
 }
--- scripts/insurer.js
+++ scripts/insurer.js
@@ -1,6 +1,23 @@
 var Market = artifacts.require("Market");
+var insurer = process.argv[4];
+console.log('Hi, I am insurer', insurer);
 
 module.exports = function(callback) {
-  console.log('insurer', Market.deployed())
-  callback()
+  var events;
+  Market.deployed().then(function(deployed){
+     events = deployed.allEvents({})
+     events.watch(function(err,result) {
+       if (result.event == 'QuoteRequestedEvent') {
+         console.log('Got request from ', result.args.name, 'for $', result.args.cover.toString(), 'Million')
+         setTimeout(function(){
+           console.log('Sending my quote ....')
+           deployed.response('0x0', insurer, 20)
+           callback()
+         }, 3000);
+       }else{
+        //  console.log('unrelated', result.event)
+       }
+     })
+  })
+  // console.log('eol')
 }
